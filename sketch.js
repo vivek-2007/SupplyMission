0a21e6f44584f466107028418a2e6fe0f2abe4eb
vivek-2007 <83000441+vivek-2007@users.noreply.github.com>
2021-05-05 19:13:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.rectangle(width/2 , 200 , 10, 10 ,{restitution:0.4, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 ,{restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
 	
 
